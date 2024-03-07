0ff906157ec4823eee4a293a2514ad2eb7df138c
Doanhaiduy <haiduytbt2k3@gmail.com>
2024-03-07 17:54:04 +0700

    ts paths

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
-import DayListItem from '../../src/components/core/DayListItem';
-import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
-import { AmaticSC_400Regular, AmaticSC_700Bold } from '@expo-google-fonts/amatic-sc';
+import { FlatList, StyleSheet, View } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import DayListItem from '@/components/core/DayListItem';
 
 SplashScreen.preventAutoHideAsync();
 
